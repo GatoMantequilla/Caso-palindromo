cbc4e02603cf16a2dd4b65c5b910a257210b1ac7
Vass0 <e.huidobro01@ufromail.cl>
2024-09-08 15:24:47 -0300

    Pequeño cambio al test.

--- PalindromosTest.java
+++ PalindromosTest.java
@@ -1,8 +1,10 @@
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class PalindromosTest {
 
-    @org.junit.jupiter.api.Test
+    @Test
     void esPalindromo() {
 
         assertTrue(Palindromos.esPalindromo("aca"));
